6c831558423962a811f115552ae6dcfacf7e7930
tomike19 <kolajoelizabeth@gmail.com>
2022-01-20 23:43:25 -0800

    restructuring the folder

---
+++ IntroToReact/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+const App = () => {
+  const name="peter";
+  const age="10";
+  return (
+    <div>
+    <p>hello {name}</p> 
+    <p>peter is {age} years old </p>
+    </div>
+  );
+}
+
+export default App;
