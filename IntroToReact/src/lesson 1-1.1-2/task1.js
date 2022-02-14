09f54d17229b5597b37d4737b9487f4bfd20fd0d
tomike19 <kolajoelizabeth@gmail.com>
2022-02-13 22:41:03 -0800

    learning object methods and this

--- IntroToReact/src/lesson 1-1.1-2/task1.js
+++ IntroToReact/src/lesson 1-1.1-2/task1.js
@@ -1,33 +1,44 @@
 import React from 'react';
-import Header from '../lesson 1-1.1-2/header';
-import Content from '../lesson 1-1.1-2/Content';
+// import Header from '../lesson 1-1.1-2/header';
+// import Content from '../lesson 1-1.1-2/Content';
 // import Total from '../lesson 1-1.1-2/Total';
 // import Part1 from './Part1';
 
 
 const Task1 = () => {
- const course = 'Half Stack application development'
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10
-  }
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7
-  }
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14
-  }
+  const arto = {
+  name: 'Arto Hellas',
+  age: 35,
+  education: 'PhD',
+  greet: function() {
+    console.log('hello, my name is ' + this.name)
+  },
+}
+
+arto.greet() 
+//  const course = 'Half Stack application development'
+//   const part1 = {
+//     name: 'Fundamentals of React',
+//     exercises: 10
+//   }
+//   const part2 = {
+//     name: 'Using props to pass data',
+//     exercises: 7
+//   }
+//   const part3 = {
+//     name: 'State of a component',
+//     exercises: 14
+//   }
 
     return (
         <div>
             
-<Header course={course} />
+{/* <Header course={course} />
     <Content parts={parts} />
-    <Total parts={parts}  />
+    <Total parts={parts}  /> */}
         </div>
     );
 }
 
 export default Task1;
+
