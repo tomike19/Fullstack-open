c1385d9754fdf32b2b8cf186425a4c05d00778b6
tomike19 <kolajoelizabeth@gmail.com>
2022-02-17 13:10:30 -0800

    event handling

--- IntroToReact/src/App.js
+++ IntroToReact/src/App.js
@@ -5,14 +5,20 @@ import { useState } from 'react';
 const App = () => {
    const [ counter, setCounter ] = useState(0)
 
-  setTimeout(
-    () => setCounter(counter + 1),
-    1000
-  )
-  
+  // setTimeout(
+  //   () => setCounter(counter + 100),
+  //   1000
+  // )
+  const handleClick =()=> {
+    console.log('hello')
+  }
+  console.log('rendering...', counter)
   return (
     <div>
     {counter}
+     <button onClick={handleClick}>
+        plus
+      </button>
     </div>
   );
 }
