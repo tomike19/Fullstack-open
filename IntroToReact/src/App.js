f28c9f4815ff3db14531c12be1460cf6a0b27454
tomike19 <kolajoelizabeth@gmail.com>
2022-02-17 12:02:04 -0800

    stateful components

--- IntroToReact/src/App.js
+++ IntroToReact/src/App.js
@@ -1,11 +1,18 @@
 import './App.css';
-import Task1 from './lesson 1-1.1-2/task1';
+import { useState } from 'react';
+// import Task1 from './lesson 1-1.1-2/task1';
 
 const App = () => {
+   const [ counter, setCounter ] = useState(0)
+
+  setTimeout(
+    () => setCounter(counter + 1),
+    1000
+  )
   
   return (
     <div>
-    <Task1/>
+    {counter}
     </div>
   );
 }
